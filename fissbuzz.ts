c1aaeb75bdfc888cdc8183e0e2bfc1a4664197c4
Ruddy <130799823+RuddyKay@users.noreply.github.com>
2025-04-12 01:03:31 -0700

    Add files via upload

---
+++ fissbuzz.ts
@@ -0,0 +1,15 @@
+function checkFizzBuzz(num: number): void {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("fizzbuzz");
+    } else if (num % 3 === 0) {
+        console.log("fizz");
+    } else if (num % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(num);
+    }
+}
+
+for (let i = 1; i <= 100; i++) {
+    checkFizzBuzz(i);
+}
